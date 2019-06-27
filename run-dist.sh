17e0cf4cb4afd3384096c4a6f06fb9655b167fa6
Jeremy Laviole <poqudrof@gmail.com>
2019-06-27 16:46:40 +0200

    updated arguments

--- run-dist.sh
+++ run-dist.sh
@@ -1,5 +1,5 @@
 
 CP=$(</usr/share/natar/natar-camera-client/classpath.txt)
-java -Xmx64m -cp $CP:target/* tech.lity.rea.nectar.CameraTest --input camera0 
+java -Xmx64m -cp $CP:target/* tech.lity.rea.nectar.CameraTest $@
 
 
